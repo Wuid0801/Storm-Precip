bfd2cea89a65d99d6bc881cfa38530b5055e35d0
hskang <hskang@anchors-biz.com>
2026-04-06 15:48:35 +0900

    feat: logo 추가

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import faviconUrl from './assets/logo.svg?url';
+
+let faviconLink = document.querySelector("link[rel='icon']");
+if (!faviconLink) {
+  faviconLink = document.createElement('link');
+  faviconLink.rel = 'icon';
+  faviconLink.type = 'image/svg+xml';
+  document.head.appendChild(faviconLink);
+}
+faviconLink.href = faviconUrl;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
